dc09ab678c78edbf7adf5b6c9b65e9fa4413784e
Mateuzo <mateushcandido5@gmail.com>
2024-05-05 16:11:17 -0300

    finishing project

--- sabor_expresso/app.py
+++ sabor_expresso/app.py
@@ -19,57 +19,87 @@ def show_options():
     print('3. Ativar restaurante')
     print('4. Sair \n')
 
-def finish_app():
-    os.system('cls')
-    print('finalziando app\n')
-
-def invalid_option():
-    print('opção inválida\n')    
-    input('Digite alguma tecla para voltar para o menu principal: ')
-    
-    os.system('cls')
-
-    show_options()
-    chosen_option()
-
-def register_new_restaurant():
-        os.system('cls')
-        print('CADASTRO DE NOVOS RESTAURANTES\n')
-
-        restaurant_name = input('Digite o nome do restaurante: ')
-        restaurants.append(restaurants)
-        print(f'O nome do restaurante cadastro é {restaurant_name}\n')
-
-        input('digite uma tecla para voltar para o menu principal\n')
-
-        main()
-
 def chosen_option():
     try:
         chosed_option =  int(input('Escolha uma opção: '))
     
         if chosed_option == 1:
             register_new_restaurant()
+            back_to_menu()
         elif chosed_option == 2:
-            print('Listar restaurantes')
+            list_restaurants(restaurants)
+            back_to_menu()
         elif chosed_option == 3:
             print('Ativar restaurante')
+            active_restaurant(restaurants)
+            back_to_menu()
         elif chosed_option == 4:
+
             finish_app()
         else:
             invalid_option()
     except:
         invalid_option()
 
+def invalid_option():
+    print('opção inválida\n')    
+    back_to_menu()
 
+def register_new_restaurant():
+        os.system('cls')
+        print('CADASTRO DE NOVOS RESTAURANTES\n')
 
-    
+        restaurant_name = input('Digite o nome do restaurante: ')
+        restaurant_category = input('Insira a categoria do restaurnte: ')
+        restaurant_status = False
 
+        restaurant = {'name': restaurant_name, 'category': restaurant_category, 'status': restaurant_status}
+
+        restaurants.append(restaurant)
+        print(f'O nome do restaurante cadastro é {restaurant_name}\n')
+
+def list_restaurants(restaurants):
+    print('Listar restaurantes\n')
+
+    for restaurant in restaurants:
+        restaurant_name = restaurant['name']
+        restaurant_category = restaurant['category']
+        restaurant_status = restaurant['status']
+
+        print(f'NOME: {restaurant_name}   CATEGORIA: {restaurant_category}   ATIVO? {restaurant_status}\n')
+
+def active_restaurant(restaurants):
+    print(f'Listando restaurantes inativos: \n')
+
+    for index, restaurant in enumerate(restaurants):
+        if restaurant['status'] is False:
+            restaurant_name = restaurant['name']
+            restaurant_category = restaurant['category']
+            restaurant_status = restaurant['status']
+
+            print(f'{index} -  NOME: {restaurant_name} CATEGORIA: {restaurant_category} ATIVO? {restaurant_status}\n')
+        
+    chosen_restaraunt = int(input('Selecione o número do restaurante para ativar: '))
+
+    restaurants[chosen_restaraunt]['status'] = True
+
+def finish_app():
+    os.system('cls')
+    print('finalizando app\n')
 
 def main():
     show_app_name()
     show_options()
     chosen_option()
+
+def back_to_menu():
+    input('Digite alguma tecla para voltar para o menu principal: ')
+    os.system('cls')
+
+    show_options()
+    chosen_option()
+
+
 
 if __name__ == '__main__':
     main()
